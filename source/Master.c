ce8bd1d2b5ec5afba94c6745f5206733ee6b5ca3
Pixie Zenne <231278179+pixie-zenne@users.noreply.github.com>
2026-04-03 20:27:10 +0800

    Create Master.c

---
+++ source/Master.c
@@ -0,0 +1,15 @@
+#define master main
+
+/********************************
+ *  @author pixie-zenne
+ *  @version 0.0.0 WIP
+ *  @license Apache License 2.0
+ ********************************/
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int master(void) {
+  return 0;
+}
